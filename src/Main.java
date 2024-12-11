d2a51fa9289c0b0fd8a6fc7a32812f1213f8225f
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2024-12-11 10:40:37 +0100

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        // Steg 1
+        String[] leksaker = {"Pokéball", "Legendary scar nerf gun", "Flowberry plush", "Barbie", "Bilar"};
+
+        // Steg 2
+        /*for (int i = 0; i < leksaker.length; i++) {
+            System.out.println(leksaker[i]);
+        }*/
+        for (String leksak : leksaker) {
+            System.out.println(leksak);
+        }
+
+        // Steg 3 & 4
+        String[] klasskamrater = {"Eirich", "Lodwog", "Bilip", "Zuttis", "Ostis"};
+        int[] nummer = {4, 8, 1, 7, 3};
+
+        for (int i = 0; i < klasskamrater.length; i++) {
+            System.out.println(klasskamrater[i] + " ger leksaken " + leksaker[i] + " betyget: " + nummer[i]);
+        }
+
+        // Sista stegen
+        ArrayList<String> cities = new ArrayList<>();
+        while (true) {
+            System.out.println("What city do you want to add to the list, write exit to end the loop");
+            System.out.println(">>> ");
+            String cityAnswer = sc.nextLine();
+            if (cityAnswer.equalsIgnoreCase("exit")) {
+                break;
+            }
+            cities.add(cityAnswer);
+        }
+        for (String city : cities) {
+            System.out.println(city);
+        }
     }
 }
